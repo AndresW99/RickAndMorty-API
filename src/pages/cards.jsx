036c146d5aed3026caedea7843311ef1af660a2f
Andres <wellmann39@gmail.com>
2021-07-11 12:00:33 -0600

    Responsive casi terminado

--- src/pages/cards.jsx
+++ src/pages/cards.jsx
@@ -6,19 +6,18 @@ export const Cards = ( props ) => {
 
 
     return (
-
         
-        <div className="card border-info bg-secondary mb-3 estilo card-group">
+        <div className="card border-info bg-secondary mb-3 estilo animate__animated animate__fadeInLeft">
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src={ props.chars.image } className="img-fluid rounded-start" alt="" />
+                    <img src={ props.chars.image } className="img-fluid card-img-top rounded-start" alt="Rick and morty" />
                 </div>
                 <div className="col-md-8">
-                    <div className="card-body">
-                    <h5 className="card-title card-header">{ props.chars.name }</h5>
-                    <h6>{ <b>Especie</b> }: &nbsp; { props.chars.species }</h6>
-                    <h6>{ <b>Estatus</b> }: &nbsp; { props.chars.status }</h6>
-                    <h6>{ <b>Genero</b> }: &nbsp; { props.chars.gender }</h6>
+                    <div className="card-body ">
+                    <h3 className="card-title text-warning">{ props.chars.name }</h3>
+                    <h6>{ <b>Especie</b> }: { props.chars.species }</h6>
+                    <h6>{ <b>Estatus</b> }: { props.chars.status }</h6>
+                    <h6>{ <b>Genero</b> }:  { props.chars.gender }</h6>
                     </div>
                 </div>
             </div>
